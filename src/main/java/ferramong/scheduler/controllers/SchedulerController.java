0b5ed5213f0b557ce4e7b2c7adf45b911d1b38ba
William Niemiec <williamniemiec@hotmail.com>
2021-03-26 10:49:31 -0300

    Init

--- src/main/java/ferramong/scheduler/controllers/SchedulerController.java
+++ src/main/java/ferramong/scheduler/controllers/SchedulerController.java
@@ -1,14 +1,22 @@
 package ferramong.scheduler.controllers;
 
 import ferramong.scheduler.entities.Scheduler;
+import ferramong.scheduler.models.Person;
+import ferramong.scheduler.models.Post;
 import ferramong.scheduler.services.SchedulerService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotBlank;
-import java.util.List;
+import java.lang.reflect.Member;
+import java.net.URI;
+import java.util.*;
 
 /*
 * Controller
@@ -16,7 +24,7 @@ import java.util.List;
 *	Serve para chamar serviços
 *	Mapeia endpoints
 * */
-
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 public class SchedulerController {
@@ -26,5 +34,105 @@ public class SchedulerController {
     @RequestMapping("/listall/{name}")
     public List<Scheduler> listAllWithName(@PathVariable("name") @NotBlank String name) {
         return schedulerService.listAllWithName(name);
+    }
+
+    /**
+     * Gets scheduled visits within a date range.
+     *
+     * @param       start Start range
+     * @param       end End range
+     *
+     * @return      JSON with scheduled visits in the interval [start; end]
+     */
+    //@RequestMapping("/listall/{name}")
+    public List<Scheduler> getDatesWithScheduling(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date start,
+                                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date end) {
+        List<Scheduler> l = new ArrayList<>();
+        return l;
+    }
+
+    /**
+     * Gets scheduled visits for a specific day.
+     *
+     * @param       day Day whose visits will be obtained
+     *
+     * @return      JSON with scheduled visits in the interval [start; end]
+     */
+    //@RequestMapping("/listall/{name}")
+    public List<Scheduler> getSchedulerForDay(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date day) {
+        List<Scheduler> l = new ArrayList<>();
+        return l;
+    }
+
+    /**
+     * Gets all schedules that a dweller did.
+     *
+     * @param       cpf Dweller's cpf
+     *
+     * @return      JSON with the dweller's schedules with the informed cpf
+     */
+    //@RequestMapping("/listall/{name}")
+    public List<Scheduler> getByCPF(String cpf) {
+        List<Scheduler> l = new ArrayList<>();
+        return l;
+    }
+
+    /**
+     * Schedules a visit.
+     *
+     * <h2>CURL example<h2/>
+     * <code>
+     *      curl "http://localhost:8080/schedule" \
+     *      -X POST \
+     *      -d "{\n  \"cpf\": \"12345678900\", \n  \"date\": \"2021-03-26T10:35:00.000Z\"\n}" \
+     *      -H "Content-type: application/json"
+     * </code>
+     *
+     * @param       scheduling Scheduling information
+     *
+     * @return      JSON with response code
+     */
+    @CrossOrigin
+    @PostMapping(
+            value = "/schedule",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Scheduler> schedule(@RequestBody Scheduler scheduling) {
+
+        return ResponseEntity
+                .created(URI
+                        .create(String.format("/persons/%s", scheduling.getCpf())))
+                .body(scheduling);
+    }
+
+    // Matheus fará
+    /**
+     * Schedules a visit.
+     *
+     * <h2>CURL example<h2/>
+     * <code>
+     *      curl "http://localhost:8080/schedule" \
+     *      -X DELETE \
+     *      -d "{\n  \"cpf\": \"12345678900\", \n  \"date\": \"2021-03-26T10:35:00.000Z\"\n}" \
+     *      -H "Content-type: application/json"
+     * </code>
+     *
+     * @param       scheduling Scheduling information
+     *
+     * @return      JSON with response code
+     */
+    @CrossOrigin
+    @DeleteMapping(
+            value = "/schedule",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Scheduler> unschedule(@RequestBody Scheduler scheduling) {
+
+        return ResponseEntity
+                .created(URI
+                        .create(String.format("/persons/%s", scheduling.getCpf())))
+                .body(scheduling);
     }
 }
--- src/main/java/ferramong/scheduler/repositories/SchedulerRepository.java
+++ src/main/java/ferramong/scheduler/repositories/SchedulerRepository.java
@@ -13,15 +13,25 @@ import java.util.List;
  *      Responsável por gerenciar queries do banco de dados
  */
 
+/*
+*  getByID(id do usuario) que retorne qual horário ele ja tem marcado se houver,
+* um post que receba um horário e um id e salve isso no banco, e um post com id
+* do usuario para deleção do horário dele.
+*
+*  códigos http, mas ja adianto que vou usar query parameters pra mandar o id da pessoa no getById,
+*  e que por enquanto as respostas esperadas pensei em 200 no caso de ja haver a data e 204
+* no caso de não haver data para aquela pessoa.
+* */
+
 @Repository
 public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
 
     @Query(
             "SELECT s FROM Scheduler s " +
-            "WHERE s.name LIKE CONCAT(UPPER(:name), '%')"
+            "WHERE s.cpf LIKE CONCAT(UPPER(:cpf), '%')"
     )
     public List<Scheduler> listAllWithName(
-            @Param("name") String name
+            @Param("cpf") String cpf
     );
 
 }
