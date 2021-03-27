f34dad73578fc86bfe8ae0c9d2b29ac5b28cf889
Renan Kummer <dev.renankummer@outlook.com>
2021-03-26 22:53:27 -0300

    Change request http method

--- src/main/java/ferramong/scheduler/controllers/SchedulerController.java
+++ src/main/java/ferramong/scheduler/controllers/SchedulerController.java
@@ -149,7 +149,7 @@ public class SchedulerController {
      * @return      Accepted request (202) if scheduling has been successful;
      * otherwise, returns bad request (400) if an error occurred
      */
-    @PutMapping(
+    @PostMapping(
             value = "/scheduler",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
