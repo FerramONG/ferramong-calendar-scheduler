6f178a0adb6947648168ed406ede907aaf3c025a
Renan Kummer <dev.renankummer@outlook.com>
2021-03-26 21:52:46 -0300

    Add dependency and configure OpenAPI

---
+++ src/main/java/ferramong/scheduler/config/OpenApiConfig.java
@@ -0,0 +1,7 @@
+package ferramong.scheduler.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "FerramONG Scheduler API"))
+public class OpenApiConfig { }
